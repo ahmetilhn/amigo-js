8f86e7c6b86dc3d80c6ce3ba47a91eac23658003
ahmetilhn <ahmetilhan.dev@gmail.com>
2024-04-28 15:34:53 +0300

    added deep-clone util

---
+++ lib/utils/deep-clone.util.ts
@@ -0,0 +1,23 @@
+const deepClone = (val: any): typeof val => {
+  if (val === null || typeof val !== "object") {
+    return val;
+  }
+
+  if (Array.isArray(val)) {
+    const clonedArr: any[] = [];
+    for (let i = 0; i < val.length; i++) {
+      clonedArr[i] = deepClone(val[i]);
+    }
+    return clonedArr;
+  } else {
+    const clonedObj: Record<string, any> = {};
+    for (const key in val) {
+      if (Object.prototype.hasOwnProperty.call(val, key)) {
+        clonedObj[key] = deepClone(val[key]);
+      }
+    }
+    return clonedObj;
+  }
+};
+
+export default deepClone;
