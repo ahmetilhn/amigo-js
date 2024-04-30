1b60aa029d641d5ea3faeedf9e156583d8e20d9b
ahmetilhn <ahmetilhan.dev@gmail.com>
2024-04-30 03:46:35 +0300

    added jsdoc to sleep.module

--- lib/modules/sleep.module.ts
+++ lib/modules/sleep.module.ts
@@ -1,3 +1,8 @@
+/**
+ *
+ * @param {number} time - time for delay(typeof ms)
+ * @returns {Promise}
+ */
 export const sleep = async (time: number): Promise<void> => {
   return new Promise((resolve: () => void) => setTimeout(resolve, time));
 };
