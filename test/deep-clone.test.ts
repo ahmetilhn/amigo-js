6d2bfdfa32b1380eec18a4814a9b64f5953dcdce
ahmetilhn <ahmetilhan.dev@gmail.com>
2024-04-30 15:23:29 +0300

    updated deep clone test

--- test/deep-clone.test.ts
+++ test/deep-clone.test.ts
@@ -4,11 +4,13 @@ describe("Deep clone tests", () => {
   test("should return string after deepClone", () => {
     const clonedVal = deepClone("Ahmet");
     expect(clonedVal).toBe("Ahmet");
+    expect(clonedVal).toEqual(clonedVal);
   });
 
   test("should return object after deepClone", () => {
     const clonedVal = deepClone({ name: "test" });
-    expect(clonedVal).toEqual({ name: "test" });
+    expect(clonedVal).not.toBe({ name: "test" });
+    expect(clonedVal).toEqual(clonedVal);
   });
 
   test("should return object after deepClone", () => {
@@ -16,18 +18,21 @@ describe("Deep clone tests", () => {
       product: { info: { title: "Product title" } },
     };
     const clonedVal = deepClone(mockData);
+    expect(clonedVal).not.toBe(mockData);
     expect(clonedVal).toEqual(mockData);
   });
 
   test("should return array after deepClone", () => {
     const mockData = ["Test 1", "Test 2"];
     const clonedVal = deepClone(mockData);
+    expect(clonedVal).not.toBe(mockData);
     expect(clonedVal).toEqual(mockData);
   });
 
   test("should return object of array after deepClone", () => {
     const mockData = [{ title: "Test 1" }];
     const clonedVal = deepClone(mockData);
+    expect(clonedVal).not.toBe(mockData);
     expect(clonedVal).toEqual(mockData);
   });
 });
