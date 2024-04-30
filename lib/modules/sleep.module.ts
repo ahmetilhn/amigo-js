56ca238cd693a349698c8ffc981737c89cfbfe61
ahmetilhn <ahmetilhan.dev@gmail.com>
2024-04-30 03:55:13 +0300

    added jsdoc to somewhere

--- lib/modules/sleep.module.ts
+++ lib/modules/sleep.module.ts
@@ -1,7 +1,8 @@
 /**
- *
  * @param {number} time - time for delay(typeof ms)
  * @returns {Promise}
+ * @example
+ * await sleep(500) // Promise
  */
 export const sleep = async (time: number): Promise<void> => {
   return new Promise((resolve: () => void) => setTimeout(resolve, time));
--- lib/modules/type.module.ts
+++ lib/modules/type.module.ts
@@ -7,10 +7,27 @@ import {
 } from "../constants/node.constant";
 import getNode from "../helpers/get-node.helper";
 
+/**
+ * @param {*} val - value to be processed
+ * @returns {boolean} - if input is array return true else return false
+ * @example
+ * isArray([]) // true
+ * isArray({}) // false
+ * isArray("test") // false
+ */
 export const isArray = (val: any): boolean => {
   return Array.isArray(val) || getNode(val) === ARRAY_NODE;
 };
-
+/**
+ *
+ * @param {*} val - value to be processed
+ * @returns {boolean} - if input is object return else return false
+ * @example
+ * isObject(true) // false
+ * isObject([]) // true
+ * isObject({}) // true
+ * isObject("sss") // false
+ */
 export const isObject = (val: any): boolean => {
   return val !== null && (typeof val === "object" || typeof val === "function");
 };
